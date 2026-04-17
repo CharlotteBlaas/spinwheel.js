4291f74b646d947affac82f3ce6be2887622aa08
CharlotteBlaas <charlotte.blaas@touchincentive.com>
2026-04-17 10:26:00 +0200

    Update spinwheel.js

--- spinwheel.js
+++ spinwheel.js
@@ -44,7 +44,7 @@
 
     function createConfetti() {
       confettiLayer.innerHTML = '';
-      var colors = ['#d8791f', '#f3b562', '#f7e7ce', '#8f4f24'];
+      var colors = ['#FF4B4B', '#FFFFFF', '#21468B', '#FF8C00'];
       var pieces = 140;
 
       for (var i = 0; i < pieces; i++) {
